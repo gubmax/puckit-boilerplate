6d39c881d94b274740b7b24245d61dba79ddbfa9
gubmax <gubyshev@gmail.com>
2021-03-10 21:32:19 +0300

    Babel config

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = function(api) {
+  api.cache.using(() => process.env.NODE_ENV);
+
+  return {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      ['@babel/plugin-transform-runtime', {
+        corejs: false,
+        helpers: true,
+        regenerator: true,
+      }],
+    ],
+  };
+};
